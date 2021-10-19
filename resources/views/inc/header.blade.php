6e182f0dddb1f4384393249cdea82aaccb259894
Eugene Glazyrin <Eugeneglazyrin25@gmail.com>
2021-10-19 12:42:45 +0400

    fixes

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -4,13 +4,12 @@
         @guest
             @if (Route::has('login'))
                     <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                        @auth
-                            <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                    @auth
                         @else
-                            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+                            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Логин</a>
 
                             @if (Route::has('register'))
-                                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Регистрация</a>
                             @endif
                         @endauth
                     </div>
